d786b9f92cf134585fb3928191632ee0f67e7d60
Neo Gan <neo.gan2015@gmail.com>
2020-12-18 18:15:16 -0800

    Create index for project

--- dist/app.js
+++ dist/app.js
@@ -1,16 +1,1 @@
 "use strict";
-var button = document.getElementById("add");
-var input1 = document.getElementById("input1");
-var input2 = document.getElementById("input2");
-var alertEle = document.getElementById("alert");
-var answerInput = document.getElementById("value");
-button.addEventListener("click", function (e) {
-    e.preventDefault();
-    if (!input1.value || !input2.value) {
-        alertEle.style.display = "block";
-        setTimeout(function () { return (alertEle.style.display = "none"); }, 3500);
-        return;
-    }
-    answerInput.value = JSON.stringify(parseInt(input1.value) + parseInt(input2.value));
-});
-//# sourceMappingURL=app.js.map
